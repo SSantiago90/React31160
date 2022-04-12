d156e3f6e2f1acf2bedd92cd62c0531d68cc6657
Santiago <salkin.santiago@gmail.com>
2022-04-12 00:27:23 -0300

    desafio navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Mi proyecto</h1>       
+    <div>
+      <header>
+       <NavBar/>
       </header>
+      <section>
+        
+        <h1 className='className="text-3xl font-bold underline"'>
+          Mi proyecto
+        </h1>       
+      </section>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+function NavBar() {
+  return (
+    <nav className="border-2 border-rose-500 border-gray-200 drop-shadow-md">
+        <h1 className="">Mi App</h1>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/">Coder</a>
+            </li>
+            <li>
+                <a href="/">Catálogo</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
