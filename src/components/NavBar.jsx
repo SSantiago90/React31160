d362c5516023b5cf6a58b9861290966dbae3c95e
Santiago <salkin.santiago@gmail.com>
2022-04-12 14:00:37 -0300

    navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar(){
+    return(
+    <nav>
+        <h2>MiTienda</h2>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+                <a href="/">Catálogo</a>
+            </li>
+        </ul>
+    </nav>
+    )
+}
+
+export default NavBar;
